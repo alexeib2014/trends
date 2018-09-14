6a8d1b2ea642252d045ddf7e3f0fac54f9a49aca
Alexei Bushuev <alexeib.2018@gmail.com>
2018-09-14 14:21:29 +0300

    developed React frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      value: '',
+      data: []
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSearch() {
+    axios.get('/api/trends?keyword=' + this.state.value)
+         .then(response => {
+            this.setState({ data: response.data.default.timelineData })
+            console.log(response)
+          })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <button onClick={this.handleSearch}>
+          Search
+        </button>
+        <table>
+          { this.state.data.map(row => <tr>
+                                          <td>
+                                            { row.formattedAxisTime }
+                                          </td>
+                                          <td>
+                                            { row.formattedValue[0] }
+                                          </td>
+                                        </tr>) }
+        </table>
       </div>
     );
   }
